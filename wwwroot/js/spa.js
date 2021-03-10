7d200cf9702c8906171dc4364cae68bc9981eec7
sssenjovu <simon.ssenjovu@students.plymouth.ac.uk>
2021-03-10 15:18:13 +0000

    improved code

---
+++ wwwroot/js/spa.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#map').click(function () {
+        $('#page-content-wrapper').load('map.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#map').click(function () {
+        $('#page-content-wrapper').load('treasure.html');
+    })
+});
+
+$(document).ready(function () {
+    $('#map').click(function () {
+        $('#page-content-wrapper').load('data.html');
+    })
+});
